d2db948d0ba3e1ecf9383d16cba4d7a41b48caa2
Andrew Lewin <andrew@fanfair.fm>
2017-10-27 16:29:32 -0700

    added post to slack back in

--- src/io/estrado/Pipeline.groovy
+++ src/io/estrado/Pipeline.groovy
@@ -1,12 +1,23 @@
 #!/usr/bin/groovy
 package io.estrado;
-
+import groovy.json.JsonOutput
 
 def kubectlTest() {
     // Test that kubectl can correctly communication with the Kubernetes API
     println "checking kubectl connnectivity to the API"
     sh "kubectl get nodes"
 
+}
+
+def notifySlack(text, channel, slackURL, emoji) {
+    if (emoji == null) {
+      emoji = ":thumbsup:"
+    }
+    def payload = JsonOutput.toJson([text      : text,
+                                     channel   : channel,
+                                     username  : "jenkins",
+                                     icon_emoji: emoji])
+    sh "curl -X POST --data-urlencode \'payload=${payload}\' ${slackURL}"
 }
 
 def helmLint(String chart_dir) {
